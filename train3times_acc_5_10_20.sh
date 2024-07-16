66ec6e81dd0f366836665d9af7ea3f1c3938b692
morilab_lzhang <141390154+lzhang30@users.noreply.github.com>
2024-07-16 16:43:38 +0900

    Add files via upload

---
+++ train3times_acc_5_10_20.sh
@@ -0,0 +1,8 @@
+bash train3times_10p.sh -c 0 -t acc_s -m simarfor -e '_re' -l 3e-2 -w 0.1
+bash train3times_20p.sh -c 0 -t acc_s -m simarfor -e '_re' -l 3e-2 -w 0.1
+bash train3times_5p.sh -c 0 -t acc_s -m simarfor -e '_re' -l 3e-2 -w 0.1
+
+
+
+
+
